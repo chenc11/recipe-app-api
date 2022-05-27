60f1d78016e5abce4c0dd2b2cbf4024805daf983
zzz <zzz@zzzs-MacBook-Air.local>
2022-05-27 12:00:17 -0500

    Configured docker compose and checks to wait for db

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,12 @@
 Calculator functions
 """
 
+
 def add(x, y):
     """Add x and y and return results."""
     return x + y
 
+
 def subtract(x, y):
     """Subtract x from y and return results."""
-    return y - x   
+    return y - x
